dce38c6360946f58b1b171f06b9c56a45acc278c
Trunova Viktoria <vikit2000@ukr.net>
2022-10-26 23:54:22 +0300

    done-unchecked

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
 
-console.log(galleryItems);
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </div>`
+    }).join('');
+}
+
+gallery.addEventListener('click', onGalleryItem);
+let instance = '';
+
+function onGalleryItem(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">`); 
+    instance.show();
+
+    document.addEventListener('keydown', modalCloseWithEscape);
+function modalCloseWithEscape(event) {
+        if (event.code === 'Escape') {
+            document.removeEventListener('keydown', modalCloseWithEscape);
+            instance.close();
+            console.log(event);
+        }
+    }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        />
+        </a>`
+    }).join('');
+}
+
+gallery.addEventListener('click', onGalleryItem);
+
+function onGalleryItem(event) {
+    event.preventDefault();
+    let galleryLightbox = new SimpleLightbox('.gallery .gallery__item', { captionsData: 'alt', captionDelay: 250 });
+galleryLightbox.on('show.simplelightbox');
+}
